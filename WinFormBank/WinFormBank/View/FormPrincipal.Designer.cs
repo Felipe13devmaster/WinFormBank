84dc9849fd2af6b3661c8ef01c575a948521b2a5
Felipe Rodrigues <felipe13rodrigues.fr@gmail.com>
2020-06-09 17:01:25 -0300

    Inseri no formulario de cadastro os campos usuario e senha

--- WinFormBank/WinFormBank/View/FormPrincipal.Designer.cs
+++ WinFormBank/WinFormBank/View/FormPrincipal.Designer.cs
@@ -75,6 +75,14 @@
             this.label14 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label26 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label27 = new System.Windows.Forms.Label();
+            this.label28 = new System.Windows.Forms.Label();
             this.panelLogin.SuspendLayout();
             this.panelCadastro.SuspendLayout();
             this.panelConta.SuspendLayout();
@@ -102,18 +110,18 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(299, 222);
+            this.button2.Location = new System.Drawing.Point(287, 222);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(87, 30);
             this.button2.TabIndex = 6;
             this.button2.Text = "Cadastre-se";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(198, 222);
+            this.button1.Location = new System.Drawing.Point(183, 222);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(90, 30);
             this.button1.TabIndex = 5;
             this.button1.Text = "Entrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -165,6 +173,14 @@
             // panelCadastro
             // 
             this.panelCadastro.BackColor = System.Drawing.SystemColors.Control;
+            this.panelCadastro.Controls.Add(this.label28);
+            this.panelCadastro.Controls.Add(this.label27);
+            this.panelCadastro.Controls.Add(this.textBox9);
+            this.panelCadastro.Controls.Add(this.label26);
+            this.panelCadastro.Controls.Add(this.textBox7);
+            this.panelCadastro.Controls.Add(this.textBox6);
+            this.panelCadastro.Controls.Add(this.label25);
+            this.panelCadastro.Controls.Add(this.label24);
             this.panelCadastro.Controls.Add(this.button5);
             this.panelCadastro.Controls.Add(this.button4);
             this.panelCadastro.Controls.Add(this.button3);
@@ -184,16 +200,16 @@
             this.panelCadastro.Controls.Add(this.label5);
             this.panelCadastro.Controls.Add(this.label4);
             this.panelCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panelCadastro.Location = new System.Drawing.Point(155, 50);
+            this.panelCadastro.Location = new System.Drawing.Point(105, 12);
             this.panelCadastro.Name = "panelCadastro";
-            this.panelCadastro.Size = new System.Drawing.Size(630, 456);
+            this.panelCadastro.Size = new System.Drawing.Size(696, 519);
             this.panelCadastro.TabIndex = 1;
             this.panelCadastro.Visible = false;
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(358, 381);
+            this.button5.Location = new System.Drawing.Point(354, 478);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(90, 30);
             this.button5.TabIndex = 17;
@@ -203,7 +219,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(249, 381);
+            this.button4.Location = new System.Drawing.Point(249, 478);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(90, 30);
             this.button4.TabIndex = 16;
@@ -213,7 +229,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(138, 381);
+            this.button3.Location = new System.Drawing.Point(138, 478);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 30);
             this.button3.TabIndex = 15;
@@ -223,7 +239,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(138, 237);
+            this.comboBox1.Location = new System.Drawing.Point(138, 228);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 28);
             this.comboBox1.TabIndex = 14;
@@ -231,7 +247,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(138, 328);
+            this.textBox5.Location = new System.Drawing.Point(138, 299);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(438, 26);
             this.textBox5.TabIndex = 13;
@@ -245,7 +261,7 @@
             // 
             // maskedTextBox3
             // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(138, 279);
+            this.maskedTextBox3.Location = new System.Drawing.Point(138, 265);
             this.maskedTextBox3.Name = "maskedTextBox3";
             this.maskedTextBox3.Size = new System.Drawing.Size(169, 26);
             this.maskedTextBox3.TabIndex = 11;
@@ -274,7 +290,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(41, 331);
+            this.label11.Location = new System.Drawing.Point(41, 302);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(48, 20);
             this.label11.TabIndex = 7;
@@ -283,7 +299,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(42, 282);
+            this.label10.Location = new System.Drawing.Point(42, 268);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(58, 20);
             this.label10.TabIndex = 6;
@@ -292,7 +308,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(42, 240);
+            this.label9.Location = new System.Drawing.Point(42, 231);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(31, 20);
             this.label9.TabIndex = 5;
@@ -566,15 +582,81 @@
             this.label12.TabIndex = 0;
             this.label12.Text = "Nome";
             // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(42, 339);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(64, 20);
+            this.label24.TabIndex = 18;
+            this.label24.Text = "Usuario";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(42, 372);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(56, 20);
+            this.label25.TabIndex = 19;
+            this.label25.Text = "Senha";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(138, 336);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(281, 26);
+            this.textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(138, 369);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(163, 26);
+            this.textBox7.TabIndex = 21;
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(42, 402);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(56, 20);
+            this.label26.TabIndex = 23;
+            this.label26.Text = "Senha";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(138, 401);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(163, 26);
+            this.textBox9.TabIndex = 24;
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Location = new System.Drawing.Point(326, 372);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(119, 20);
+            this.label27.TabIndex = 25;
+            this.label27.Text = "Crie sua senha.";
+            // 
+            // label28
+            // 
+            this.label28.AutoSize = true;
+            this.label28.Location = new System.Drawing.Point(326, 402);
+            this.label28.Name = "label28";
+            this.label28.Size = new System.Drawing.Size(155, 20);
+            this.label28.TabIndex = 26;
+            this.label28.Text = "Confirme sua senha.";
+            // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(932, 535);
-            this.Controls.Add(this.panelConta);
-            this.Controls.Add(this.panelCadastro);
             this.Controls.Add(this.panelLogin);
+            this.Controls.Add(this.panelCadastro);
+            this.Controls.Add(this.panelConta);
             this.Name = "FormPrincipal";
             this.Text = "FuBank";
             this.panelLogin.ResumeLayout(false);
@@ -643,5 +725,13 @@
         private System.Windows.Forms.Label label19;
         private System.Windows.Forms.Label label23;
         private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label28;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label24;
     }
 }
